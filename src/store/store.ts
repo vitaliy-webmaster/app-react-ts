43a42d8a9c3cc9898569ef65ec56971c153aa273
Vitaliy Chernyshenko <vitaliy.webmaster@gmail.com>
2022-07-04 19:03:26 +0300

    Change redux-logic with redux-thunk middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import { thunk } from 'redux-thunk';
+import thunk from 'redux-thunk';
 
-const reducer = (state = null, action: any) => {
+const listsReducer = (state = null, action: any) => {
 	return state;
 };
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+	lists: listsReducer
+});
+
+const middleware = [thunk];
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(logicMiddleware)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;
